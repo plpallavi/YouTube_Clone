a69653d581b369f550eb3008858d8f546568e81a
plpallavi <pallavi121202@gmail.com>
2023-04-20 22:52:24 +0530

    Clone

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import{ Paper, IconButton} from '@mui/material';
+import {SearchIcon} from '@mui/icons-material'
+
+const SearchBar = () => {
+  return (
+    <div>SearchBar</div>
+  )
+}
+
+export default SearchBar
