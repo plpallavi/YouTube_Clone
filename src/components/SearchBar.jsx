7d6dad5838eff1d91c1dc731a226115c8f9c926c
plpallavi <pallavi121202@gmail.com>
2023-04-21 23:09:57 +0530

    Clone

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,29 @@
 import {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import{ Paper, IconButton} from '@mui/material';
-import {SearchIcon} from '@mui/icons-material'
+import {Search} from '@mui/icons-material';
 
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
+    return (
+        <Paper
+        components="form"
+        onSubmit={() =>{}}
+        sx={{
+            borderRadius:20,
+            border: '1px solid #e3e3e3',
+            pl:2,
+            boxShadow: 'none',
+            mr:{sm:5}
+        }}>
+        <input 
+            className="search-bar"
+            placeholder="Search..."
+            value=""
+            onChangr={() =>{}}/>
+        <IconButton type="submit" sx={{p:'10px', color: 'red'}}>
+            <Search />
+        </IconButton>
+        </Paper>
+    )
 }
-
 export default SearchBar
