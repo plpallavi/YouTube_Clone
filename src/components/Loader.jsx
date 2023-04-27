aeff26c136372172d231262e04a6caa60b5782b7
plpallavi <pallavi121202@gmail.com>
2023-04-27 19:28:22 +0530

    Clone

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, CircularProgress, Stack } from '@mui/material';
 
 const Loader = () =>  (
-  <Box minHeight="95vh">
+  <Box minHeight="95vh" width="100%">
     <Stack direction='row' justifyContent='center' alignItems='center' height='80vh' >
       <CircularProgress />
     </Stack>
